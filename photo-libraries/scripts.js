e9ddcd475ba67d5a73a9129a29ba1f1c553327d7
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2015-11-21 17:28:29 -0800

    got inner zoom and lens zoom working

--- photo-libraries/scripts.js
+++ photo-libraries/scripts.js
@@ -4,6 +4,23 @@ $(function (){
 $("#elevate-zoom-meg").ezPlus();
 $("#elevate-zoom-emma").ezPlus();
 
+$("#elevate-zoom-meg-inner").ezPlus({ 
+	zoomType	: "inner", 
+	cursor: "crosshair"});
+$("#elevate-zoom-emma-inner").ezPlus({ 
+	zoomType	: "inner", 
+	cursor: "crosshair"});
+
+$('#meg-lens').ezPlus({
+    zoomType: 'lens',
+    lensShape: 'round',
+    lensSize: 200});
+
+$('#emma-lens').ezPlus({
+    zoomType: 'lens',
+    lensShape: 'round',
+    lensSize: 200});
+
 });
 
 
