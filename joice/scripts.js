b36354f6408fd20a1b70243786095a585dbb6e90
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2015-11-26 09:33:14 -0800

    saving changes here

--- joice/scripts.js
+++ joice/scripts.js
@@ -1,4 +1,37 @@
-$(function (){
+$(function() {
 	console.log("Jquery is loading!");
+
+	// Test surveyurvey test via prompts
+
+	var questions = [{
+		question: "How are you doing today?",
+		choices: ["good", "great"]
+	}, {
+		question: "I'll ask you a few questions in order to make a recipe recommendation.  Sound good?",
+		choices: ["yep"]
+	}, {
+		question: "Are you a vegetarian?",
+		choices: ["yes", "no"]
+	}, {
+		question: "Do you like cheese?",
+		choices: ["yes", "no"]
+	}, {
+		question: "How much time do you have to prepare the meal?",
+		choices: ["<30 minutes", ">30 minutes"]
+	}, {
+		question: "Thanks for all the information.  Are you ready for a recommendation?",
+		choices: ["Yep!"]
+	}];
+
+	var currentQuestion = 0;
+	var selections = [];
+	var survey = $('#question');
+
+	displayNext();
+
+	// Click handler for the 'next' button
+	$('.next').on('click', function(e) {
+		e.preventDefault();
+	});
 
 });
