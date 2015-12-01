696092f6be75baeb0e98135297e6b9078f7dac72
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2015-12-01 08:38:52 -0800

    some more formatting

--- joice/scripts.js
+++ joice/scripts.js
@@ -23,8 +23,6 @@ $(function() {
 		choices: ["Yep!"]
 	}];
 
-	var $divClone = $('#quiz-container').clone();
-
 	var $profile = $('<div class="icon"><img id="joice" src="images/joice-chat.png"></div>');
 	var $greeting = $('<div class="text"><p>Happy to help. How are you doing today?</p></div>');
 	var $vegetarian = $('<div class="text"><p>Are you a vegetarian?</p></div>');
@@ -46,6 +44,20 @@ $(function() {
 	var $lessThirty = $('<div><button type="button" id="lessThanThirty" class="btn btn-primary">Less than 30 min</button></div>');
 	var $moreThirty = $('<div><button type="button" id="moreThanThirty" class="btn btn-primary">More than 30 min</button></div>');
 
+	var $beginbtn2 = $('<button type="button" id="begin" class="btn btn-primary">Ready!</button>');
+	
+	var $greatbtn2 = $('<button type="button" id="great" class="btn btn-primary">Great</button>');
+	var $goodbtn2 = $('<button type="button" id="good" class="btn btn-primary">Good</button>');
+
+	var $yesVege2 = $('<div><button type="button" id="yes" class="btn btn-primary">Yes.  Vegetarian</button></div>');
+	var $noVege2 = $('<div><button type="button" id="no" class="btn btn-primary">No.  I eat meat</button></div>');
+
+	var $yesCheese2 = $('<div><button type="button" id="yesToCheese" class="btn btn-primary">Yes to cheese</button></div>');
+	var $noCheese2 = $('<div><button type="button" id="noToCheese" class="btn btn-primary">No to cheese</button></div>');
+
+	var $lessThirty2 = $('<div><button type="button" id="lessThanThirty" class="btn btn-primary">Less than 30 min</button></div>');
+	var $moreThirty2 = $('<div><button type="button" id="moreThanThirty" class="btn btn-primary">More than 30 min</button></div>');
+
 	var $zucchini = $('<img class="solution" src="images/zucchini.jpg">');
 	var $eggplant = $('<img class="solution" src="images/eggplant.jpg">');
 	var $fajitas = $('<img class="solution" src="images/fajitas.jpg">');
@@ -53,17 +65,14 @@ $(function() {
 	var $likeButton = $('<button type="button" id="like" class="btn btn-default">Like</button>');
 	var $likedButton = $('<button type="button" id="like" class="btn btn-primary">Like</button>');
 
-	var $moreButton = $('<button type="button" id="like" class="btn btn-primary">More</button>');
-	// var currentQuestion = 0;
-	// var selections = [];
-	// var survey = $('#question');
-
-	// displayNext();
+	var $moreButton = $('<button type="button" class="btn btn-primary">More</button>');
+	var $moreButton2 = $('<button type="button" class="btn btn-primary">More</button>');
+ 
 
 	// Click handler to start the quiz
 
 	$("#begin").click(function() {
-		$("#div2").html($beginbtn);
+		$("#div2").html($beginbtn2);
 		$("#icon3").html($profile);
 		$("#text3").html($greeting);
 		$("#left").html($greatbtn);
@@ -71,7 +80,7 @@ $(function() {
 
 	});
 	$greatbtn.click(function() {
-		$("#div4").html($greatbtn);
+		$("#div4").html($greatbtn2);
 		$("#icon5").html($profile);
 		$("#text5").html($vegetarian);
 		$("#left").empty();
@@ -81,7 +90,7 @@ $(function() {
 
 	});
 	$goodbtn.click(function() {
-		$("#div4").html($goodbtn);
+		$("#div4").html($goodbtn2);
 		$("#icon5").html($profile);
 		$("#text5").html($vegetarian);
 		$("#left").empty();
@@ -93,7 +102,7 @@ $(function() {
 	$yesVege.click(function() {
 		$("#icon7").html($profile);
 		$("#text7").html($cheese);
-		$("#div6").html($yesVege);
+		$("#div6").html($yesVege2);
 		$("#left").empty();
 		$("#right").empty();
 		$("#left").html($yesCheese);
@@ -102,7 +111,7 @@ $(function() {
 	});
 	$noVege.click(function() {
 		$("#div5").html($cheese);
-		$("#div6").html($noVege);
+		$("#div6").html($noVege2);
 		$("#icon7").html($profile);
 		$("#text7").html($cheese);
 		$("#left").empty();
@@ -113,7 +122,7 @@ $(function() {
 	});
 	$yesCheese.click(function() {
 		$("#div9").html($time);
-		$("#div8").html($yesCheese);
+		$("#div8").html($yesCheese2);
 		$("#icon9").html($profile);
 		$("#text9").html($cheese);
 		$("#left").empty();
@@ -124,7 +133,7 @@ $(function() {
 	});
 	$noCheese.click(function() {
 		$("#div9").html($time);
-		$("#div8").html($noCheese);
+		$("#div8").html($noCheese2);
 		$("#icon9").html($profile);
 		$("#text9").html($cheese);
 		$("#left").empty();
@@ -135,7 +144,7 @@ $(function() {
 	});
 	$moreThirty.click(function() {
 		$("#div11").html($solution);
-		$("#div10").html($moreThirty);
+		$("#div10").html($moreThirty2);
 		$("#icon11").html($profile);
 		$("#text11").html($cheese);
 		$("#div12").html($zucchini);
@@ -147,7 +156,7 @@ $(function() {
 	});
 	$lessThirty.click(function() {
 		$("#div11").html($solution);
-		$("#div10").html($lessThirty);
+		$("#div10").html($lessThirty2);
 		$("#icon11").html($profile);
 		$("#div12").html($zucchini);
 		$("#left").empty();
@@ -161,9 +170,14 @@ $(function() {
 	});
 	// more button
 	$moreButton.click(function() {
-		console.log("the more button was selected");
 		$("#div12").html($fajitas);
-
+		$("#right").empty();
+		$("#right").html($moreButton2);
+	});
+	$moreButton2.click(function() {
+		$("#div12").html($eggplant);
+		$("#right").empty();
+		$("#right").html($moreButton);
 	});
 
 });
